6158cacb71d840bcd65efa909361464f85214aa5
Thomas Jakway <tjakway@nyu.edu>
2016-06-21 13:26:31 -0400

    began writing buffer stack class

---
+++ bufstack.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import vim 
+
+class BufferStackDict(object):
+    def __init__(self):
+        self.bufdict = dict()
+        self.default_key = "default"
+
+    #if the default key isn't in the dictionary,
+    #add it with an empty stack
+    def remake_default(self):
+        if not self.default_key in self.bufdict:
+            self.bufdict[self.default_key] = list()
+
+    #returns None if the stack for the passed window has no valid buffers
+    #***WARNING:*** assumes the window is a valid key in the dictionary
+    #explicitly pass self.default_key if this is not the case
+    def _pop_next_valid_buf(self, window):
+        while len(win_stack) > 0:
+            #python's pop() with no args returns the _last_ item in the list...
+            return_buf = win_stack.pop(0)
+            if return_buf.valid:
+                return return_buf
+
+        return None
+             
+
+    def new_window_stack(self, window):
+        if window not in self.bufdict:
+            self.bufdict[window] = list()
+
+    def del_window_stack(self, window):
+        if window in self.bufdict:
+            del self.bufdict[window]
+        
+    def push_buf(self, window, buf):
+        if window in self.bufdict:
+            self.bufdict[window].insert(0, buf)
+        else:
+            self.bufdict[self.default_key].insert(0, buf)
+
+    def pop_buf(self, window):
+        #if this window has its own non-empty stack, pop that buffer
+        if window in self.bufdict and len(self.bufdict[window]) > 0:
+            return self._pop_next_valid_buf(window)
+        #if this window doesn't have its own stack, pop from the default stack
+        else:
+            return self._pop_next_valid_buf(self.default_key)
+
