b9eb86621061c7f37ddffccb05b3aa8225faa7c6
Thomas Jakway <tjakway@nyu.edu>
2016-06-28 06:28:16 -0400

    outlining more tests

--- python/BufstackTests.py
+++ python/BufstackTests.py
@@ -2,6 +2,8 @@ import unittest
 from Bufstack import BufferStackDict
 from random import randint
 
+
+
 #emulate a vim buffer's members
 class MockBuffer:
     def __init__(self, number, valid):
@@ -9,13 +11,7 @@ class MockBuffer:
         self.valid = valid
 
     @classmethod
-    def RandBuffers(cls, num):
-        #arbitrary, but highly unlikely we'll have >100 buffers
-        upper_bound = 100
-
-        if num > upper_bound:
-            raise "Will not generate >100 random buffers"
-
+    def RandBuffers(cls, num, upper_bound):
         rand_numbers = list()
         for i in range(num):
             rand_num = randint(0, upper_bound)
@@ -35,20 +31,32 @@ class MockWindow:
 class BufstackTest(unittest.TestCase):
     def setUp(self):
         self.bufstack = BufferStackDict()
+        self.max_rand_buffers = 200   
+
+    def push_rand_bufs(self, num_rand_buffers):
+        for i in MockBuffer.RandBuffers(num_rand_buffers, self.max_rand_buffers):
+            self.bufstack.push(MockWindow(-1), i)
 
 class PushValidBufsTest(BufstackTest):
     def setUp(self): 
         BufstackTest.setUp(self)
 
     def runTest(self):
-        num_rand_buffers = 100
-        for i in MockBuffer.RandBuffers(num_rand_buffers):
-            self.bufstack.push(MockWindow(-1), i)
+        self.push_rand_bufs(100)
+        self.assertEqual(len(self.bufstack.get_stack_for_window(MockWindow(-1))), 100)
 
-        self.assertEqual(len(self.bufstack.get_stack_for_window(MockWindow(-1))), num_rand_buffers)
+class RandPushPopsTest(BufstackTest):
+    def setUp(self): 
+        BufstackTest.setUp(self)
+
+    def runTest(self):
+        pass
 
 
-#class DefaultIndexTests(BufstackTest):
-#    def setUp(self): 
-#        BufstackTest.setUp(self)
-#    def runTest(self):
+class DefaultIndexTests(BufstackTest):
+    def setUp(self): 
+        BufstackTest.setUp(self)
+
+    def runTest(self):
+         
+        for buf, win in (MockBuffer.RandBuffers(100, self.max_rand_buffers), )
