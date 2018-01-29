88fc7ecb8c0e38a8291425c665fb7a5f65a1a44e
Thomas Jakway <tjakway@nyu.edu>
2018-01-29 09:24:04 -0800

    I think getenv just accepts the variable name, not the $ or include/CommonDefines.hpp

--- include/CommonDefines.hpp
+++ include/CommonDefines.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+//pretty sure getenv will take "PATH" on both platforms...
+//try %PATH% on windows if that doesn't work
+#define PATH_VAR_NAME "PATH"
 #ifdef _WIN32
-#define PATH_VAR_NAME "%PATH%"
 #define PATH_VAR_SEPARATOR ";"
 #define PATH_SEPARATOR "\\"
 #else
-#define PATH_VAR_NAME "PATH"
 #define PATH_VAR_SEPARATOR ":"
 #define PATH_SEPARATOR "/"
 #endif
