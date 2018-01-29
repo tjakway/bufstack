2ec9e55146c7a3f3d76aeb51c0b5790e664cadf0
Thomas Jakway <tjakway@nyu.edu>
2018-01-29 11:47:36 -0800

    reorganized ReadApiInfoTests

--- test/src/ReadApiInfoTests.cpp
+++ test/src/ReadApiInfoTests.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
 
 #include <msgpack.hpp>
-#include <iostream>
+#include <functional>
+#include <ostream>
 
 #include "NamespaceDefines.hpp"
 #include "Server.hpp"
 #include "FindNeovim.hpp"
 #include "MockServer.hpp"
+#include "Util/Util.hpp"
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -18,26 +20,62 @@ BUFSTACK_BEGIN_NAMESPACE
 class ReadApiInfoTests : public ::testing::Test
 {
 public:
-    const std::string apiInfoFilename {"resources/api_info"};
+    int readFd;
+    static const std::string apiInfoFilename;
+
+    //can't use assertions in CTOR
+    ReadApiInfoTests()
+        : readFd(-1)
+    { }
+
+    virtual void SetUp()
+    {
+        readFd = open(ReadApiInfoTests::apiInfoFilename.c_str(), O_RDONLY);
+        ASSERT_GT(readFd, 0) << std::string(strerror(errno));
+        return;
+    }
+
+    ~ReadApiInfoTests()
+    {
+        if(Util::fd_is_valid(readFd))
+        {
+            close(readFd);
+        }
+    }
+
+
+    void readExpect(std::function<bool(const std::vector<msgpack::object_handle>&)> expect)
+    {
+        bool receivedMessage = false;
+        bool expected = false;
+        const auto callback = [&receivedMessage, &expected, expect](const std::vector<msgpack::object_handle>& vecH)
+        { 
+            if(!vecH.empty())
+            {
+                receivedMessage = true;
+            }
+
+            bool passed = expect(vecH);
+            if(passed)
+            {
+                expected = true;
+            }
+        };
+        MockServer server;
+        server.readFd(readFd, callback);
+        EXPECT_TRUE(receivedMessage);
+        EXPECT_TRUE(expected);
+    }
 };
+const std::string ReadApiInfoTests::apiInfoFilename {"resources/api_info"};
+
 
 //make sure we can decode it without crashing
 TEST_F(ReadApiInfoTests, TestCallbackInvoked)
 {
-    int readFd = open(apiInfoFilename.c_str(), O_RDONLY);
-    ASSERT_GT(readFd, 0) << strerror(errno);
-
-    bool receivedMessage;
-    const auto callback = [&receivedMessage](const std::vector<msgpack::object_handle>& vecH)
-    { 
-        if(!vecH.empty())
-        {
-            receivedMessage = true;
-        }
-    };
-    MockServer server;
-    server.readFd(readFd, callback);
-    ASSERT_TRUE(receivedMessage);
+    readExpect([](const std::vector<msgpack::object_handle>&){ return true; });
 }
+
+
 
 BUFSTACK_END_NAMESPACE
