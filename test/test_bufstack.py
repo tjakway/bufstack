6a82c9d797bb7efe16ab59e4b72e0f5b01ac7896
Thomas Jakway <tjakway@nyu.edu>
2018-02-01 13:58:53 -0800

    wrote bufstack_assert_rpc_call

--- test/test_bufstack.py
+++ test/test_bufstack.py
@@ -25,3 +25,28 @@ def test_identify_windows():
 
     vim.run_loop(request_cb, None, setup_cb)
 
+
+#can't call it test_something or nosetests will try to run it
+def bufstack_assert_rpc_call(rpc_function_name, before_state, after_state):
+    plugin = BufstackPlugin(vim)
+
+    def setup_cb():
+        before_state(plugin)
+        cmd = 'let g:result = rpcrequest({}, "{}")'.format(cid, rpc_function_name)
+        vim.command(cmd)
+        vim.stop_loop()
+
+    def request_cb(name, args):
+        eq(name, rpc_function_name)
+
+        #stop the loop so the message gets processed
+        vim.stop_loop()
+        after_state(plugin)
+
+    vim.run_loop(request_cb, None, setup_cb)
+
+@with_setup(setup=cleanup)
+def bufstack_assert_rpc_call():
+    test_bufstack_rpc_call("BufstackPerTabPageStacks",
+            lambda plugin: eq(plugin.identify_tab_pages, False),
+            lambda plugin: eq(plugin.identify_tab_pages, True))
