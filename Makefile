51fc537bfd3f270f4397530c58f5a70a498333ad
Thomas Jakway <tjakway@nyu.edu>
2018-02-01 08:12:05 -0800

    wrote test_identify_windows

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 .PHONY: all
 all: check
 
+.PHONY: clean
 clean:
 	find . -name "*.pyc" -type f -delete
 
+.PHONY: check
 check:
 	PYTHONPATH="./rplugin/python3:$$PYTHONPATH" nosetests
+
+tags:
+	ctags -R . || /bin/true
--- test/test_bufstack.py
+++ test/test_bufstack.py
@@ -9,6 +9,19 @@ cid = vim.channel_id
 @with_setup(setup=cleanup)
 def test_identify_windows():
     plugin = BufstackPlugin(vim)
-    eq(plugin.identify_windows, False)
-    cmd = 'rpcrequest(%d, "BufstackPerWindowStacks")' % cid
-    eq(plugin.identify_windows, True)
+
+    def setup_cb():
+        eq(plugin.identify_windows, False)
+        cmd = 'let g:result = rpcrequest(%d, "BufstackPerWindowStacks")' % cid
+        vim.command(cmd)
+        vim.stop_loop()
+
+    def request_cb(name, args):
+        eq(name, "BufstackPerWindowStacks")
+
+        #stop the loop so the message gets processed
+        vim.stop_loop()
+        eq(plugin.identify_windows, True)
+
+    vim.run_loop(request_cb, None, setup_cb)
+
