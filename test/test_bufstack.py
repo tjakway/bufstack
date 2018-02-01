f1f9576c5288ae80f5ccb205f7e249a99d768f6c
Thomas Jakway <tjakway@nyu.edu>
2018-02-01 07:47:16 -0800

    added plugin argument

--- test/test_bufstack.py
+++ test/test_bufstack.py
@@ -1,5 +1,5 @@
-import unittest
 import random
+import neovim
 from nose.tools import with_setup, eq_ as eq
 from test_common import *
 from BufstackPlugin import *
@@ -8,7 +8,7 @@ cid = vim.channel_id
 
 @with_setup(setup=cleanup)
 def test_identify_windows():
-    plugin = BufstackPlugin()
+    plugin = BufstackPlugin(vim)
     eq(plugin.identify_windows, False)
     cmd = 'rpcrequest(%d, "BufstackPerWindowStacks")' % cid
     eq(plugin.identify_windows, True)
