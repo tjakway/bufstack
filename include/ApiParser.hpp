20d37fa8c151194386801e8b57aa9ad86a2a27a7
Thomas Jakway <tjakway@nyu.edu>
2018-08-14 11:51:28 -0700

    writing hash specialization for NvimFunction

--- include/ApiParser.hpp
+++ include/ApiParser.hpp
@@ -2,13 +2,15 @@
 
 #include "NamespaceDefines.hpp"
 #include "Loggable.hpp"
-#include "NewExceptionType.hpp"
+#include "Util/NewExceptionType.hpp"
 
 #include <msgpack.hpp>
 
+#include <functional>
 #include <map>
 #include <set>
 #include <vector>
+#include <unordered_set>
 #include <string>
 #include <algorithm>
 
@@ -53,6 +55,33 @@ public:
     }
 };
 
+BUFSTACK_END_NAMESPACE
+
+//from https://stackoverflow.com/questions/5889238/why-is-xor-the-default-way-to-combine-hashes/27952689#27952689
+size_t hash_combine( size_t lhs, size_t rhs ) {
+  lhs^= rhs + 0x9e3779b9 + (lhs << 6) + (lhs >> 2);
+  return lhs;
+}
+
+//hash specialization
+namespace std
+{
+    template<> struct hash<bufstack::NvimFunction>
+    {
+        typedef bufstack::NvimFunction argument_type;
+        typedef std::size_t result_type;
+        result_type operator()(argument_type const& f) const noexcept
+        {
+            return hash_combine(std::hash<const optional<std::string>>{}(f.returnType),
+                    hash_combine(std::hash<const optional<std::string>>{}(f.sinceVersion),
+                    hash_combine(std::hash<const std::vector<std::string>>{}(f.parameters),
+                        std::hash<const std::string>{}(f.name));
+        }
+    };
+}
+
+BUFSTACK_BEGIN_NAMESPACE
+
 class ApiParser : public Loggable
 {
     const msgpack::object_handle& handle;
@@ -81,7 +110,9 @@ public:
 
     static std::vector<std::string> extractFunctionNames(const msgpack::object_handle&);
 
-    static std::vector<NvimFunction> parseFunctions(const msgpack::object_handle&);
+    static std::unordered_set<NvimFunction> parseFunctions(
+            const std::vector<msgpack::object_handle>&);
+    static NvimFunction parseFunction(const msgpack::object_handle&);
 
 public:
     ApiParser(const msgpack::object_handle& _handle)
