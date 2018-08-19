6414cb2a1fcb3829606c6cfb4477f76249c0b363
Thomas Jakway <tjakway@nyu.edu>
2018-08-18 17:38:40 -0700

    fixup! added MsgpackRpc::Message type

--- include/MsgpackRpc.hpp
+++ include/MsgpackRpc.hpp
@@ -2,39 +2,60 @@
 
 #include <functional>
 
+#include <msgpack.hpp>
+
 #include <cstdint>
+
 #include "nonstd/optional.hpp"
+
+#include "Util/NewExceptionType.hpp"
+
+using namespace nonstd;
 
 class MsgpackRpc
 {
+public:
+    NEW_EXCEPTION_TYPE(MsgpackRpcException);
+
     class Message
     {
+    private:
+        NEW_EXCEPTION_TYPE_WITH_BASE(MessageFormatException, 
+                MsgpackRpcException);
+        void checkCtorArgs();
+
+    public:
         enum Type
         {
-            Request = 0;
-            Response = 1;
-            Notification = 2;
+            Request = 0,
+            Response = 1,
+            Notification = 2
         };
         const Type type;
 
         const uint32_t msgId;
         const optional<std::string> error;
 
-        const std::reference_wrapper<msgpack::object> result;
+         
+        const optional<std::reference_wrapper<msgpack::object>> result;
+
+        const std::vector<std::reference_wrapper<msgpack::object>> params;
 
         Message(Type _type, 
                 uint32_t _msgId, 
                 optional<std::string> _error,
-                std::reference_wrapper<msgpack::object> _result)
+                optional<std::reference_wrapper<msgpack::object>> _result,
+                std::vector<std::reference_wrapper<msgpack::object>> _params)
             : type(_type), msgId(_msgId), error(_error),
-            result(_result)
+            result(_result), params(_params)
         {}
 
         Message(const Message& other)
-            : type(other.type),
-            msgId(other.msgId),
-            error(other.error),
-            result(other.result)
+            : Message(other.type,
+            other.msgId,
+            other.error,
+            other.result,
+            other.params)
         {}
     };
 };
