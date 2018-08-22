318759c6c3a3762bf2c1f698c611ee55c5b77cec
Thomas Jakway <tjakway@nyu.edu>
2018-08-22 14:21:35 -0700

    fixup! separated library configuration from target-specific functions

--- cmake/depends_rpclib.cmake
+++ cmake/depends_rpclib.cmake
@@ -12,19 +12,9 @@ endif()
 
 function(depends_rpclib TARGET_NAME_PARAM)
     if(USE_SYSTEM_RPCLIB)
-        #note: system rpclib is untested
-        find_package(rpc 2.2.1 REQUIRED)
-
         target_include_directories(${TARGET_NAME_PARAM} SYSTEM INTERFACE ${MSGPACK_INCLUDE_DIRS})
         target_link_libraries(${TARGET_NAME_PARAM} INTERFACE ${MSGPACK_LIBRARIES})
     else()
-        set(RPCLIB_CXX_STANDARD 11 CACHE BOOL "Require C++11" FORCE)
-
-
-        add_subdirectory(${CMAKE_SOURCE_DIR}/lib/rpclib 
-            ${CMAKE_BINARY_DIR}/rpclib
-            EXCLUDE_FROM_ALL)
-
        #target_include_directories(${TARGET_NAME_PARAM} SYSTEM PUBLIC
        #    ${CMAKE_SOURCE_DIR}/lib/rpclib/include)
 
