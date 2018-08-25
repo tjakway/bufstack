254e0b2691c42f625bd21cbab7f708e1c97e852b
Thomas Jakway <tjakway@nyu.edu>
2018-08-24 19:12:45 -0700

    wrote RemoteApiFunction

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "Util/Util.hpp"
+#include "Util/NewExceptionType.hpp"
 #include "NamespaceDefines.hpp"
 #include "ApiParser.hpp"
+
+#include "nonstd/optional.hpp"
     
 #include <cstdint>
 
@@ -12,7 +15,53 @@
 //NOTE: must include msgpack before rpc
 #include <rpc/client.h>
 
+using namespace nonstd;
+
 BUFSTACK_BEGIN_NAMESPACE
+
+class Client;
+
+template <typename T, typename... Args>
+class RemoteApiFunction
+{
+    optional<NvimFunction> functionSpecification;
+    std::shared_ptr<Client> client;
+
+protected:
+    RemoteApiFunction(optional<NvimFunction> spec,
+            std::shared_ptr<Client> _client)
+        : functionSpecification(spec),
+        client(_client)
+    {}
+
+    NEW_EXCEPTION_TYPE(RemoteApiFunctionError);
+    NEW_EXCEPTION_TYPE_WITH_BASE(NotInApiInfoError, RemoteApiFunctionError);
+
+    /**
+     * throw a NotInApiInfoError if the api info doesn't contain the 
+     * passed function specification
+     */
+    void checkApiInfo(const ApiInfo&);
+
+public:
+    RemoteApiFunction(const NvimFunction& spec, 
+            std::shared_ptr<Client> client
+            const ApiInfo& info)
+        : RemoteApiFunction(make_optional(spec), client)
+    {
+        checkApiInfo(info);
+    }
+
+
+    //ctor that skips the api info check
+    RemoteApiFunction(std::shared_ptr<Client> client)
+        : RemoteApiFunction(nullopt, client)
+    {}
+
+    //TODO: implement
+    T call(const Args&... args);
+    T async_call(const Args&... args);
+};
 
 class Client
 {
@@ -31,6 +80,7 @@ public:
         : address(_address), port(_port),
             client(make_unique<rpc::client>(address, port))
     {}
+
 
 
 };
