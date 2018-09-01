7b4b54644a9ec6d9ca47ceeb48372957cb03b205
Thomas Jakway <tjakway@nyu.edu>
2018-09-01 11:55:30 -0700

    skeleton class for RemoteFunctionTests

---
+++ test/src/RemoteFunctionTests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include <msgpack.hpp>
+
+#include "NamespaceDefines.hpp"
+
+BUFSTACK_BEGIN_NAMESPACE
+
+class RemoteFunctionTests : public ::testing::Test, public Loggable
+{
+public:
+    RemoteFunctionTests()
+        : Loggable("RemoteFunctionTests")
+    {}
+};
+
+BUFSTACK_END_NAMESPACE
