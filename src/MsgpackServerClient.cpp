897f2bfbbf6344d6005017f13046263b328da947
Thomas Jakway <tjakway@nyu.edu>
2018-09-04 12:36:49 -0700

    wrote addResponseCallback

--- src/MsgpackServerClient.cpp
+++ src/MsgpackServerClient.cpp
@@ -7,14 +7,18 @@ BUFSTACK_BEGIN_NAMESPACE
 
 void MsgpackServerClient::addResponseCallback(BoundResponseCallback&& cb)
 {
-    const auto f = [](std::vector<BoundResponseCallback>& v, 
+    std::function<void(std::vector<BoundResponseCallback>&, 
+            BoundResponseCallback&&)> f = 
+        [](std::vector<BoundResponseCallback>& v, 
             BoundResponseCallback&& callback) -> void
     {
         v.emplace_back(callback);
     };
 
-    responseCallbacks.access(
-            std::bind(f, std::placeholders::_1, std::move(cb)));
+    auto g = [f, &cb](std::vector<BoundResponseCallback>& v){
+        return f(v, std::move(cb));
+    };
+    responseCallbacks.access<void>(g);
 }
 
 BUFSTACK_END_NAMESPACE
