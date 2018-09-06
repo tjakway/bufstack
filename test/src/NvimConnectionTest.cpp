5e64e2b0254e8867f4754b34934904411cc39660
Thomas Jakway <tjakway@nyu.edu>
2018-09-05 18:05:35 -0700

    fixup! finished implementingn NvimConnectionTest

--- test/src/NvimConnectionTest.cpp
+++ test/src/NvimConnectionTest.cpp
@@ -1,19 +1,52 @@
 #include "NvimConnectionTest.hpp"
 
 #include <msgpack.hpp>
+#include <mutex>
 
 #include "NamespaceDefines.hpp"
 #include "FindNeovim.hpp"
+#include "Client.hpp"
+#include "Util/NewExceptionType.hpp"
+#include "Util/Util.hpp"
+
+namespace {
+    static std::mutex clientPtrMutex;
+    static std::unique_ptr<bufstack::Client> clientPtr = nullptr;
+
+}
 
 BUFSTACK_BEGIN_NAMESPACE
 
-std::atomic<std::unique_ptr<Client>> NvimConnectionTest::client {nullptr};
+//std::atomic<std::unique_ptr<Client>> NvimConnectionTest::client {nullptr};
 const std::string NvimConnectionTest::localhost = "127.0.0.1";
 
-
-
-void NvimConnectionTest::connect()
+Client& NvimConnectionTest::getClientInstance()
 {
+    std::lock_guard<std::mutex> {clientPtrMutex};
+    if(!clientPtr)
+    {
+        throw NvimNotInitialized("Nvim instance not initialized "
+                "(did you remember to connect()?)");
+    }
+    else
+    {
+        return *clientPtr;
+    }
+}
+
+
+void NvimConnectionTest::connect(
+        const std::string& address,
+        uint16_t port)
+
+{
+    std::lock_guard<std::mutex> {clientPtrMutex};
+    //only connect once
+    if(clientPtr)
+    {
+        return;
+    }
+
     auto nvimDest = FindNeovim::getFirstOnPath();
     if(nvimDest == nullptr)
     {
@@ -22,7 +55,9 @@ void NvimConnectionTest::connect()
     else
     {
         const std::string nvimPath = *nvimDest;
-
+        //launch neovim then connect the client to that address and port
+        launchNeovim(nvimPath.c_str(), address, port);
+        clientPtr = make_unique<Client>(address, port);
     }
 }
 
