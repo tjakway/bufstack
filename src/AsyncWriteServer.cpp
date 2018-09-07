e083d32c3ab7acbe257742899c16fa5bf77258b1
Thomas Jakway <tjakway@nyu.edu>
2018-09-07 12:05:07 -0700

    fixup! refactored out SingleConnectionServer

--- src/AsyncWriteServer.cpp
+++ src/AsyncWriteServer.cpp
@@ -18,18 +18,6 @@
 
 BUFSTACK_BEGIN_NAMESPACE
 
-void SingleConnectionServer::onConnect(int clientFd)
-{
-    if(connected.load())
-    {
-        throw SingleConnectionServerError("Already connected to one client");
-    }
-    else
-    {
-        connected.store(true);
-    }
-    Server::onConnect(clientFd);
-}
 
 AsyncWriteServer::AsyncWriteServer(
             int backlogSize,
