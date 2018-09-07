6bb5cbba3e0d53a9e444606fe71601a7470ab66b
Thomas Jakway <tjakway@nyu.edu>
2018-09-07 14:02:14 -0700

    added BaseException

--- include/BufSender.hpp
+++ include/BufSender.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "NamespaceDefines.hpp"
+#include "Util/NewExceptionType.hpp"
 
+#include "Buffer.hpp"
 #include "Loggable.hpp"
 
 BUFSTACK_BEGIN_NAMESPACE
@@ -9,10 +11,13 @@ BUFSTACK_BEGIN_NAMESPACE
 class BufSender : virtual public Loggable
 {
 protected:
+    NEW_EXCEPTION_TYPE(BufSenderException);
     static void sendAll(int, const char* buf, ssize_t bufLen, Loggable&);
 public:
     virtual void send(int, Buffer) = 0;
     virtual void send(int, const char*, std::size_t) = 0;
+
+    using BaseException = BufSenderException;
 };
 
 BUFSTACK_END_NAMESPACE
