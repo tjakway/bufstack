023d6c630805cbedeb6bdcacd688d67c1657450e
Thomas Jakway <tjakway@nyu.edu>
2018-09-07 14:21:22 -0700

    fixed defaultBacklogSize type declaration

--- src/Config.cpp
+++ src/Config.cpp
@@ -8,7 +8,7 @@
 BUFSTACK_BEGIN_NAMESPACE
 
 //just should be something large
-const int Config::Defaults::defaultBacklogSize = 8192;
+const std::size_t Config::Defaults::defaultBacklogSize = 8192;
 
 const std::chrono::milliseconds Config::Defaults::serverSleepInterval = 
     std::chrono::milliseconds(50);
