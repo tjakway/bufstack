fbae068949ecf362453f00571692d61a8b235723
Thomas Jakway <tjakway@nyu.edu>
2018-09-07 12:53:39 -0700

    fixup! fixup! fixup! moved onConnect to new class

--- include/OnConnect.hpp
+++ include/OnConnect.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "NamespaceDefines.hpp"
+#include "Util/NewExceptionType.hpp"
 
 BUFSTACK_BEGIN_NAMESPACE
 
@@ -8,8 +9,12 @@ class Connectible
 {
 protected:
     virtual void onConnect(int fd) = 0;
+    NEW_EXCEPTION_TYPE(ConnectionException);
+
 public:
     virtual ~Connectible() {}
+
+    using BaseException = ConnectionException;
 };
 
 BUFSTACK_END_NAMESPACE
