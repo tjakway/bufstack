0ce8d7a632993b5b74387219073bafe6539e2a51
Thomas Jakway <tjakway@nyu.edu>
2018-09-08 12:17:36 -0700

    fixup! fixup! wrote SyncBufSender

--- include/AsyncBufSender.hpp
+++ include/AsyncBufSender.hpp
@@ -21,6 +21,12 @@ protected:
     virtual void send(int, Buffer) override;
     virtual void send(int, const char*, std::size_t) override;
 
+
+    //newest futures will be at the front of the queue
+    using FutureType = std::future<void>;
+    std::deque<FutureType> futures;
+    void reapFutures() noexcept;
+
     NEW_EXCEPTION_TYPE_WITH_BASE(AsyncBufSenderException, BaseException);
 
 private:
@@ -29,12 +35,6 @@ private:
     bool forceAsync;
 
     std::mutex writeMutex;
-
-    //newest futures will be at the front of the queue
-    using FutureType = std::future<void>;
-    std::deque<FutureType> futures;
-
-    void reapFutures() noexcept;
 
     void doSend(int, Buffer);
 public:
