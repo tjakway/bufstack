795c5eef5ce9b3c95271ab483b382b719957049e
Thomas Jakway <tjakway@nyu.edu>
2018-09-09 13:47:08 -0700

    moved Client functionality to MsgpackClient

--- include/MsgpackClient.hpp
+++ include/MsgpackClient.hpp
@@ -5,13 +5,29 @@
 #include "HasFd.hpp"
 #include "AbstractMsgpackClient.hpp"
 
+#include "NvimApi/RemoteFunction.hpp"
+
+#include <memory>
+
 BUFSTACK_BEGIN_NAMESPACE
 
 class MsgpackClient : 
     public AbstractMsgpackClient,
-    virtual public HasClientFd
+    virtual public HasClientFd,
+    //needed to initialize remote functions
+    public std::enable_shared_from_this<MsgpackClient>
 {
     const std::unique_ptr<ClientConnection> clientConnection;
+
+    std::unique_ptr<RemoteFunctionInstances> remoteFunctions;
+    static const std::string subscribedEvents;
+
+
+    void onConnect();
+protected:
+    void initializeRemoteFunctions(const ApiInfo&);
+    void subscribeEvents();
+    void checkFunctions(const std::unordered_set<NvimFunction>&);
 
 public:
     MsgpackClient(ConnectionInfo);
--- src/MsgpackClient.cpp
+++ src/MsgpackClient.cpp
@@ -5,8 +5,51 @@
 
 BUFSTACK_BEGIN_NAMESPACE
 
+
+const std::string MsgpackClient::subscribedEvents = 
+        "BufEnter,BufLeave,TabEnter,TabLeave,WinEnter,WinLeave";
+
 MsgpackClient::MsgpackClient(ConnectionInfo ci)
     : clientConnection(ClientConnection::newClientConnection(ci))
 {}
+
+void MsgpackClient::onConnect()
+{
+    std::function<void(void)> init = 
+        [this]() -> void {
+            msgpack::object_handle apiInfoObject = client->call("nvim_get_api_info");
+
+            ApiParser parser(apiInfoObject.get());
+            ApiInfo apiInfo = parser.getApiInfo();
+
+            //make sure function signatures match what we expect
+            std::unordered_set<NvimFunction> functions = parser.getFunctions();
+            this->checkFunctions(functions);
+
+            this->initializeRemoteFunctions(apiInfo);
+            this->subscribeEvents();
+        };
+
+    std::async(std::launch::async, init);
+//    std::future<void> x = then<msgpack::object_handle, std::function<void(msgpack::object_handle)>>
+//        (apiInfoCall, init);
+}
+
+void MsgpackClient::initializeRemoteFunctions(
+        const ApiInfo& apiInfo)
+{
+    remoteFunctions = make_unique<RemoteFunctionInstances>(
+            shared_from_this(), apiInfo);
+}
+
+void MsgpackClient::checkFunctions(const std::unordered_set<NvimFunction>&)
+{
+    //TODO
+}
+
+void MsgpackClient::subscribeEvents()
+{
+    remoteFunctions->subscribe(subscribedEvents);
+}
 
 BUFSTACK_END_NAMESPACE
