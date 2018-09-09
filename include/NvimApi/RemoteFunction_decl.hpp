ad4992f859c3de7aee357173f61e4c0e168711a8
Thomas Jakway <tjakway@nyu.edu>
2018-09-09 14:49:27 -0700

    added forward declaration header

---
+++ include/NvimApi/RemoteFunction_decl.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "NamespaceDefines.hpp"
+
+BUFSTACK_BEGIN_NAMESPACE
+
+class AbstractRemoteApiFunction;
+class HasReturnValueRemoteApiFunction;
+class NoReturnRemoteApiFunction;
+class RemoteFunctionInstances;
+
+BUFSTACK_END_NAMESPACE
