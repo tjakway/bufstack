fd142d257ee5bae02e47edbb32967f3ab87f4942
Thomas Jakway <tjakway@nyu.edu>
2018-09-09 16:26:33 -0700

    added dummy implementation of checkApiInfo

--- src/NvimApi/RemoteFunction.cpp
+++ src/NvimApi/RemoteFunction.cpp
@@ -2,5 +2,11 @@
 
 BUFSTACK_BEGIN_NAMESPACE
 
+/**
+ * throw a NotInApiInfoError if the api info doesn't contain the 
+ * passed function specification
+ */
+//TODO: implement
+void checkApiInfo(const ApiInfo&) {}
 
 BUFSTACK_END_NAMESPACE
