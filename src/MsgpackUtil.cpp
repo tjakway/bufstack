f72fbf5d9f49e0b379578558199166994bdac145
Thomas Jakway <tjakway@nyu.edu>
2018-09-09 15:29:36 -0700

    added copy method from msgpack-c repository at https://github.com/msgpack/msgpack-c

--- src/MsgpackUtil.cpp
+++ src/MsgpackUtil.cpp
@@ -10,4 +10,16 @@ std::vector<std::reference_wrapper<const msgpack::object>>
     return std::vector<std::reference_wrapper<const msgpack::object>>(v.begin(), v.end()) ;
 }
 
+/**
+ * taken from msgpack-c/include/msgpack/v1/object.hpp (in namespace detail)
+ * see MsgpackUtil.cpp header regarding license
+ */
+msgpack::object_handle MsgpackUtil::clone(msgpack::object const& obj) 
+{
+    std::size_t size = msgpack::aligned_zone_size(obj);
+    msgpack::unique_ptr<msgpack::zone> z(size == 0 ? MSGPACK_NULLPTR : new msgpack::zone(size));
+    msgpack::object newobj = z.get() ? msgpack::object(obj, *z) : obj;
+    return msgpack::object_handle(newobj, msgpack::move(z));
+}
+
 BUFSTACK_END_NAMESPACE
