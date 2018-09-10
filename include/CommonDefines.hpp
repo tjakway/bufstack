9c6bf73f5a85fe2f39b1bd3949494c62abe07ae1
Thomas Jakway <tjakway@nyu.edu>
2018-09-10 12:09:05 -0700

    replaced PATH_VAR_NAME with a FindNeovim field

--- include/CommonDefines.hpp
+++ include/CommonDefines.hpp
@@ -2,7 +2,6 @@
 
 //pretty sure getenv will take "PATH" on both platforms...
 //try %PATH% on windows if that doesn't work
-#define PATH_VAR_NAME "PATH"
 #ifdef _WIN32
 #define PATH_VAR_SEPARATOR ';'
 #define PATH_SEPARATOR "\\"
