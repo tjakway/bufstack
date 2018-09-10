96210f5b6d5faa2e4ef5619d510bb7d40340de70
Thomas Jakway <tjakway@nyu.edu>
2018-09-09 17:00:57 -0700

    added todo

--- cmake/clang_libstdcpp_hack.cmake
+++ cmake/clang_libstdcpp_hack.cmake
@@ -3,6 +3,9 @@
 #provides debug information even when it doesn't
 #this is set globally because it affects all targets
 #https://stackoverflow.com/questions/41745527/cannot-view-stdstring-when-compiled-with-clang
+#
+#TODO: refactor out the CMAKE_BUILD_TYPE check to a separate function that also
+#checks for debug-related targets like DebugWithWarnings[Optimized]
 if (CMAKE_BUILD_TYPE EQUAL "DEBUG" AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     set(CMAKE_CXX_FLAGS "-D_GLIBCXX_DEBUG ${CMAKE_CXX_FLAGS}")
 endif()
