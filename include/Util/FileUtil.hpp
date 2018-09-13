6d4c997fa154932c526cdad3f65dc2ffdc4fb2a9
Thomas Jakway <tjakway@nyu.edu>
2018-09-13 14:18:25 -0700

    fixup! fixup! fixup! wrote readBinaryFile

--- include/Util/FileUtil.hpp
+++ include/Util/FileUtil.hpp
@@ -11,23 +11,24 @@ class FileUtil
     FileUtil() = delete;
 
     using SizeType = unsigned long;
+    using CharType = char;
 
 public:
-    static std::pair<std::unique_ptr<unsigned char[]>, SizeType>
-        readBinaryFile(const std::string& filename)
+    static std::pair<std::unique_ptr<CharType[]>, SizeType>
+        readBinaryFile(const std::string& inputFile)
     {
-        ifstream in;
+        std::ifstream in;
 
         //throw an exception on error
         //see http://www.cplusplus.com/reference/ios/ios/exceptions/
         in.exceptions( std::ifstream::failbit | std::ifstream::badbit);
 
         //open at end of file to get size
-        in.open(inputFile, ios::binary | ios::in | ios::ate);
+        in.open(inputFile, std::ios::binary | std::ios::in | std::ios::ate);
 
         SizeType fileSize = in.tellg();
 
-        std::unique_ptr<unsigned char[]> buf(new unsigned char[fileSize]);
+        std::unique_ptr<CharType[]> buf(new CharType[fileSize]);
 
         //rewind
         in.seekg(std::ios::beg);
