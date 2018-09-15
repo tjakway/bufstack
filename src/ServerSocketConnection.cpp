0728f3dd68f16a3a265557991f0d081ed22f9134
Thomas Jakway <tjakway@nyu.edu>
2018-09-15 12:20:30 -0700

    wrote startListen()

--- src/ServerSocketConnection.cpp
+++ src/ServerSocketConnection.cpp
@@ -7,6 +7,7 @@
 
 #include <sys/types.h>
 
+#include <cstdlib>
 #include <cerrno>
 
 BUFSTACK_BEGIN_NAMESPACE
@@ -22,6 +23,7 @@ ServerSocketConnection::ServerSocketConnection(int serverFd,
 
 void ServerSocketConnection::startListen()
 {
+    //bind the socket and wait for a connection
     if(bind(getServerFd(), 
         (struct sockaddr*)&sockaddr,
         sizeof(sockaddr)) != 0)
@@ -40,7 +42,24 @@ void ServerSocketConnection::startListen()
             strerror(_errno)));
     }
 
-    start
+    while(!interrupted())
+    {
+        //wait for a connection then hand it off to the callback
+        sockaddr_in client;
+        memset(&client, 0 sizeof(sockaddr_in));
+        socklen_t clientLen = sizeof(sockaddr_in);
+        int clientFd = accept(server_fd, (struct sockaddr *) &client, &clientLen);
+
+        if(clientFd < 0)
+        {
+            auto _errno = errno;
+            throw ListenException(STRCATS(
+                "Error when calling accept(2): " <<
+                strerror(_errno)));
+        }
+
+        onClientConnect(clientFd);
+    }
 }
 
 BUFSTACK_END_NAMESPACE
