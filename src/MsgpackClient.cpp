cb0e102e167ad296f79d71d588cc80d3078eadd9
Thomas Jakway <tjakway@nyu.edu>
2018-09-16 12:09:22 -0700

    fixup! moved neovim-related functionality from MsgpackClient into a new class NvimClient

--- src/MsgpackClient.cpp
+++ src/MsgpackClient.cpp
@@ -5,81 +5,15 @@
 #include "NvimApi/ApiParser.hpp"
 #include "NvimApi/RemoteFunction.hpp"
 
-namespace {
-    class MsgpackOnConnectThread 
-    {
-
-    };
-}
-
 BUFSTACK_BEGIN_NAMESPACE
 
 
-const std::string MsgpackClient::subscribedEvents = 
-        "BufEnter,BufLeave,TabEnter,TabLeave,WinEnter,WinLeave";
-
-MsgpackClient::MsgpackClient(ConnectionInfo ci, bool skipOnConnect)
-    : clientConnection(ClientConnection::newClientConnection(ci))
-{
-    if(!skipOnConnect)
-    {
-        //since the client connection is constructed, we've already connected
-        onConnect();
-    }
-}
-
 MsgpackClient::MsgpackClient(ConnectionInfo ci)
-    : MsgpackClient(ci, false)
+    : clientConnection(ClientConnection::newClientConnection(ci))
 {}
 
 MsgpackClient::~MsgpackClient()
 {}
-
-void MsgpackClient::onConnect()
-{
-    /*
-    std::function<void(void)> init = 
-        [this]() -> void {*/
-        this->getLogger()->set_level(spdlog::level::debug);
-    
-        msgpack::object_handle apiInfoObject = 
-            this->call<msgpack::object_handle>("nvim_get_api_info");
-
-        this->getLogger()->debug("Received api info object");
-
-        ApiParser parser(apiInfoObject.get());
-        ApiInfo apiInfo = parser.getApiInfo();
-
-        //make sure function signatures match what we expect
-        std::unordered_set<NvimFunction> functions = parser.getFunctions();
-        this->checkFunctions(functions);
-
-        this->initializeRemoteFunctions(apiInfo);
-        this->subscribeEvents();
-    //};
-
-    //std::async(init).wait();
-//    std::future<void> x = then<msgpack::object_handle, std::function<void(msgpack::object_handle)>>
-//        (apiInfoCall, init);
-}
-
-void MsgpackClient::initializeRemoteFunctions(
-        const ApiInfo& apiInfo)
-{
-    remoteFunctions = make_unique<RemoteFunctionInstances>(
-            shared_from_this(), apiInfo);
-}
-
-void MsgpackClient::checkFunctions(const std::unordered_set<NvimFunction>&)
-{
-    //TODO
-}
-
-void MsgpackClient::subscribeEvents()
-{
-    remoteFunctions->subscribe(subscribedEvents);
-}
-
 
 ClientConnection& MsgpackClient::getClientConnection() const
 {
--- src/NvimClient.cpp
+++ src/NvimClient.cpp
@@ -1,20 +1,56 @@
 #include "NvimClient.hpp"
 
+namespace {
+
+    class MsgpackOnConnectThread 
+    {
+
+    };
+}
+
 BUFSTACK_BEGIN_NAMESPACE
+
+const std::string MsgpackClient::subscribedEvents = 
+        "BufEnter,BufLeave,TabEnter,TabLeave,WinEnter,WinLeave";
 
 void NvimClient::onConnect()
 {
+    /*
+    std::function<void(void)> init = 
+        [this]() -> void {*/
+        this->getLogger()->set_level(spdlog::level::debug);
+    
+        msgpack::object_handle apiInfoObject = 
+            this->call<msgpack::object_handle>("nvim_get_api_info");
+
+        this->getLogger()->debug("Received api info object");
+
+        ApiParser parser(apiInfoObject.get());
+        ApiInfo apiInfo = parser.getApiInfo();
+
+        //make sure function signatures match what we expect
+        std::unordered_set<NvimFunction> functions = parser.getFunctions();
+        this->checkFunctions(functions);
+
+        this->initializeRemoteFunctions(apiInfo);
+        this->subscribeEvents();
+    //};
+
+    //std::async(init).wait();
+//    std::future<void> x = then<msgpack::object_handle, std::function<void(msgpack::object_handle)>>
+//        (apiInfoCall, init);
 
 }
 
-void NvimClient::initializeRemoteFunctions(const ApiInfo&)
+void NvimClient::initializeRemoteFunctions(const ApiInfo& apiInfo)
 {
-
+    remoteFunctions = make_unique<RemoteFunctionInstances>(
+            shared_from_this(), apiInfo);
 }
 
 void NvimClient::subscribeEvents()
 {
-
+    remoteFunctions->subscribe(subscribedEvents);
 }
 
 void NvimClient::checkFunctions(const std::unordered_set<NvimFunction>&)
@@ -26,8 +62,14 @@ void NvimClient::checkFunctions(const std::unordered_set<NvimFunction>&)
     * skipOnConnect: whether to call onConnect()
     */
 NvimClient::NvimClient(ConnectionInfo ci, bool skipOnConnect)
+    : Loggable("NvimClient"),
+    MsgpackClient(ci)
 {
-
+    if(!skipOnConnect)
+    {
+        //since the client connection is constructed, we've already connected
+        onConnect();
+    }
 }
 
 NvimClient::NvimClient(ConnectionInfo ci)
