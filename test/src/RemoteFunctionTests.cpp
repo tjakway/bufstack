40736e340879a0dab85d3690b398f796e3062981
Thomas Jakway <tjakway@nyu.edu>
2018-09-17 17:37:51 -0700

    added first connection test

--- test/src/RemoteFunctionTests.cpp
+++ test/src/RemoteFunctionTests.cpp
@@ -3,16 +3,25 @@
 #include <msgpack.hpp>
 
 #include "NamespaceDefines.hpp"
+#include "NvimConnectionTest.hpp"
 #include "Loggable.hpp"
 
 BUFSTACK_BEGIN_NAMESPACE
 
-class RemoteFunctionTests : public ::testing::Test, public Loggable
+class RemoteFunctionTests 
+    : public ::testing::Test, 
+    public Loggable,
+    public NvimConnectionTest
 {
 public:
     RemoteFunctionTests()
         : Loggable("RemoteFunctionTests")
     {}
 };
+
+TEST_F(RemoteFunctionTests, TestGetClientInstance)
+{
+    auto client = getClientInstance();
+}
 
 BUFSTACK_END_NAMESPACE
