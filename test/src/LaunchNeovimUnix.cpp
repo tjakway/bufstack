cef9b04c1634b53e3b51025a72fd8163ef78f7f7
Thomas Jakway <tjakway@nyu.edu>
2018-09-17 18:57:29 -0700

    launchNeovim now creates a pipe and returns the file descriptors

--- test/src/LaunchNeovimUnix.cpp
+++ test/src/LaunchNeovimUnix.cpp
@@ -63,51 +63,58 @@ namespace {
     }
 }
 
-void NvimConnectionTest::launchNeovim(
-    const std::string& path,
-    const std::string& address,
-    uint16_t port)
+std::pair<int,int> NvimConnectionTest::launchNeovim(
+    const std::string& path)
 {
-    const std::string addrString = 
-        address + std::string(":") + std::to_string(port);
+    //create a pipe to communicate with the nvim child process
+    int pipeFds[2];
+    pipeFds[0] = -1;
+    pipeFds[1] = -1;
+
+    if(pipe(pipeFds) != 0)
+    {
+        auto _errno = errno;
+        throw NvimLaunchException(
+            STRCATS("Error calling pipe(2): " <<
+                strerror(_errno)));
+    }
+
 
     pid_t pid = fork();
     //child
     if(pid == 0)
     {
-        const char* envVar = "NVIM_LISTEN_ADDRESS";
-        int ret = setenv(envVar, addrString.c_str(), 1);
-        if(ret < 0)
+        //set the read end of the pipe to stdin and
+        //the write end to stdout
+        if(dup2(STDIN_FILENO, pipeFds[0]) == -1)
         {
-            //print an error message instead of throwing an exception
-            //because we've already forked
-            const auto _errno = errno;
-            const auto errStr = strerror(_errno);
-            const char* errMsg = STRCATS("Error calling setenv with " << envVar
-                << errStr << std::endl).c_str();
-            std::cerr << errMsg;
-            //just in case, print system wide error message
-            perror(errMsg);
-            exit(1);
+            throw NvimLaunchException(
+                STRCATS("Error setting the read end of the pipe (" <<
+                    "file descriptor " << pipeFds[0] << ") to stdin"));
         }
-
-        redirectFds("/dev/null", "/dev/null", 
-                [](std::string msg) { throw NvimLaunchException(msg); });
+        
+        if(dup2(STDOUT_FILENO, pipeFds[1]) == -1)
+        {
+            throw NvimLaunchException(
+                STRCATS("Error setting the write end of the pipe (" <<
+                    "file descriptor " << pipeFds[1] << ") to stdout"));
+        }
 
         int execRet = execl(path.c_str(), 
                 //don't forget to pass the executable path as argv[0]
                 path.c_str(), 
-                "--headless", "--noplugin", 
+                "--embed"
+                //"--noplugin", 
                 //no vimrc or shada
-                "-u", "NONE", "-i", "NONE",
+                //"-u", "NONE", "-i", "NONE",
+                "-V20my_log",
                 NULL);
         
         //only reached if there's an error in exec
         auto _errno = errno;
         throw NvimLaunchException(
             STRCATS("Error launching nvim at path " << 
-                path << " with NVIM_LISTEN_ADDRESS=" << 
-                addrString << " (execl returned " << execRet << ")"));
+                path << " (execl returned " << execRet << ")"));
     }
     else if(pid < 0)
     {
@@ -120,6 +127,7 @@ void NvimConnectionTest::launchNeovim(
     else
     {
         nvimPid = make_unique<pid_t>(pid);
+        return std::make_pair(pipeFds[0], pipeFds[1]);
     }
 }
 
