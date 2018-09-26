57f6591072bc073f7e576e55907004451736e639
Thomas Jakway <tjakway@nyu.edu>
2018-09-25 17:49:22 -0700

    fixup! initial check in

--- src/Bufstack/Core.hs
+++ src/Bufstack/Core.hs
@@ -1,9 +1,15 @@
-module Bufstack.core where
+module Bufstack.Core where
+
+import Control.Concurrent.STM
+import Neovim.API.String
 
 data Config = Config
 
 data Bufstack =
     Bufstack {
         config :: Config,
-
+        buffers :: TVar [Buffer]
         }
+
+initBufstack :: Config -> STM Bufstack
+initBufstack c = Bufstack c <$> newTVar []
