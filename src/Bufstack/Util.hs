eab9bc02744b04cb5a9e8f51c926fa19c44d09e4
Thomas Jakway <tjakway@nyu.edu>
2018-09-29 14:23:51 -0700

    wrote result returning version of modifyBuffers

--- src/Bufstack/Util.hs
+++ src/Bufstack/Util.hs
@@ -10,10 +10,31 @@ atomically :: STM.STM a -> Nvim.Neovim env a
 atomically = Nvim.liftIO . STM.atomically
 
 
-modifyBuffers :: Bufstack -> ([Nvim.Buffer] -> [Nvim.Buffer]) -> STM.STM ()
-modifyBuffers Bufstack {buffers= bufs} = STM.modifyTVar' bufs
+modifyBuffers_ :: Bufstack -> ([Nvim.Buffer] -> [Nvim.Buffer]) -> STM.STM ()
+modifyBuffers_ Bufstack {buffers= bufs} = STM.modifyTVar' bufs
 
-modifyBuffersM :: ([Nvim.Buffer] -> [Nvim.Buffer]) -> BufstackM ()
+
+modifyBuffers :: Bufstack -> 
+                ([Nvim.Buffer] -> [Nvim.Buffer]) -> 
+                STM.STM [Nvim.Buffer]
+modifyBuffers Bufstack {buffers= bufs} f = 
+        modifyBuffers_ bufs f >> readTVar bufs
+
+
+modifyBuffersMImpl :: (Bufstack -> 
+                    ([Nvim.Buffer] -> [Nvim.Buffer]) -> 
+                    STM.STM a) -> ([Nvim.Buffer] -> [Nvim.Buffer]) -> BufstackM ()
+modifyBuffersMImpl g f = 
+        let m x = atomically $ g x f
+            in Nvim.ask >>= m
+
+modifyBuffersM_ :: ([Nvim.Buffer] -> [Nvim.Buffer]) -> BufstackM ()
+modifyBuffersM_ f = 
+        let m x = atomically $ modifyBuffers_ x f
+            in Nvim.ask >>= m
+
+
+modifyBuffersM :: ([Nvim.Buffer] -> [Nvim.Buffer]) -> BufstackM [Nvim.Buffer]
 modifyBuffersM f = 
         let m x = atomically $ modifyBuffers x f
             in Nvim.ask >>= m
