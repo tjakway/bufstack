6c6aab0bbd458f739cd4e7754ee96ea58b93ec84
Thomas Jakway <tjakway@nyu.edu>
2018-09-30 16:31:03 -0700

    implemented closeAll

--- src/Bufstack/Class/Closeable.hs
+++ src/Bufstack/Class/Closeable.hs
@@ -3,8 +3,10 @@ module Bufstack.Class.Closeable where
 
 import Neovim
 import Neovim.API.String
-import Bufstack.Class.HasNumber
 import Control.Monad (foldM, mapM_)
+
+import Bufstack.Class.HasNumber
+import Bufstack.Util
 
 class Closeable a where
         close :: a -> Neovim env (Either NeovimException ())
@@ -19,22 +21,30 @@ class Closeable a where
                         in errOnInvalidResult closeNil 
 
 instance Closeable Buffer where
-        close b = getNumber b >>= (>>= (\n -> vim_command $ ":bd " ++ show n))
+        close b = getNumber b >>= (\n -> vim_command $ ":bd " ++ show n)
 
 instance Closeable Window where
-        close b = getNumber b >>= (>>= (\n -> vim_command $ ": " ++ show n ++ "quit"))
+        close b = getNumber b >>= (\n -> vim_command $ ": " ++ show n ++ "quit")
 
 
 instance Closeable Tabpage where
-        close b = vim_command (":tabclose " ++ (show . getNumber) b)
-        close' b = vim_command' (":tabclose " ++ (show . getNumber) b)
+        close b = getNumber b >>= (\n -> vim_command $ ":tabclose " ++ show n)
 
 closeAll :: Closeable a => [a] -> Neovim env (Either [NeovimException] ())
-closeAll = g . foldM f (Right ())
-    where f (Right ()) thisElem = case close thisElem of Left r -> [r]
-                                                         Right _ -> Right ()
-          f (Left errs) thisElem = case close thisElem of Left r -> Left $ r : errs
-                                                          Right _ -> Left errs
+closeAll = fmap rev . foldM f startFold
+    where f :: Closeable x => Either [NeovimException] () -> x -> Neovim env (Either [NeovimException] ())
+          f (Right ()) thisElem = do
+              n <- close thisElem
+              return $ case n of Left r -> Left [r]
+                                 Right _ -> Right ()
+
+          f (Left errs) thisElem = do
+              n <- close thisElem
+              return $ case n of Left r -> Left $ r : errs
+                                 Right _ -> Left errs
+
+          startFold :: Either [NeovimException] ()
+          startFold = return ()
 
           rev (Right ()) = Right ()
           rev (Left errs) = Left (reverse errs)
