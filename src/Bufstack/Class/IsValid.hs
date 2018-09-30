d2696cf2489fcf6dc97f5c4914648351a93b86eb
Thomas Jakway <tjakway@nyu.edu>
2018-09-30 10:12:08 -0700

    wrote IsValid typeclass

---
+++ src/Bufstack/Class/IsValid.hs
@@ -0,0 +1,20 @@
+module IsValid where
+
+import Neovim
+import Neovim.API.String
+
+class IsValid a where
+        isValid :: a -> Neovim env Bool
+        isValid' :: a -> Neovim env (Either NeovimException Bool)
+
+instance IsValid Buffer where
+        isValid = nvim_buf_is_valid
+        isValid' = nvim_buf_is_valid'
+
+instance IsValid Window where
+        isValid = window_is_valid
+        isValid' = window_is_valid'
+
+instance IsValid Tabpage where
+        isValid = tabpage_is_valid
+        isValid' = tabpage_is_valid'
