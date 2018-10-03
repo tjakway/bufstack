c454ecdec1094b900b7a2c7ce0b5722f36b6a279
Thomas Jakway <tjakway@nyu.edu>
2018-10-02 17:29:38 -0700

    wrote cleanup :: Bufstack -> IO

--- test/Bufstack/Test/Vim/Framework.hs
+++ test/Bufstack/Test/Vim/Framework.hs
@@ -5,3 +5,23 @@ module Bufstack.Test.Vim.Framework where
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
+
+import Neovim
+import Neovim.API.String
+import Control.Concurrent.STM
+import Control.Monad.Trans.Resource
+
+import Bufstack.Core
+
+cleanup :: Bufstack -> IO ()
+cleanup Bufstack{buffers = b, autocmds = a} = do
+        -- atomically clear bufstack fields
+        cmds <- atomically $ do
+            -- clear buffers
+            writeTVar b []
+            cmds <- readTVar a
+            writeTVar a []
+            return cmds
+
+        -- release autocmd resource handles
+        mapM_ release cmds
