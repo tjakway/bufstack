619c2bae1772855c04932bf50ddc46f0373ebb14
Thomas Jakway <tjakway@nyu.edu>
2018-10-04 16:34:56 -0700

    wrote nextBufFunction

--- src/Bufstack/BufferOperations.hs
+++ src/Bufstack/BufferOperations.hs
@@ -1,39 +1,80 @@
 module Bufstack.BufferOperations where
 
-import qualified Neovim as Nvim
-import qualified Neovim.API.String as Nvim
+import Neovim
+import Neovim.API.String
 import Bufstack.Core
 import Bufstack.Util
+import Bufstack.Class.HasNumber
 
-pushBuffer :: Nvim.Buffer -> BufstackM ()
+import Data.List (nub, sortBy, findIndex)
+import Data.Function (on)
+
+pushBuffer :: Buffer -> BufstackM ()
 pushBuffer buf = modifyBuffersM_ (buf :)
 
-popBuffer :: BufstackM (Maybe Nvim.Buffer)
+popBuffer :: BufstackM (Maybe Buffer)
 popBuffer = modifyBuffersMs tailMaybeS 
     where tailMaybeS [] = (Nothing, [])
           tailMaybeS (x:xs) = (Just x, xs)
 
-removeBuffer :: Nvim.Buffer -> BufstackM ()
+removeBuffer :: Buffer -> BufstackM ()
 removeBuffer b = modifyBuffersM_ $ filter (/= b)
 
 -- | look at the top buffer without removing any
-peekBuffer :: BufstackM (Maybe Nvim.Buffer)
+peekBuffer :: BufstackM (Maybe Buffer)
 peekBuffer = getBuffers >>= return . headMaybe
     where headMaybe [] = Nothing
           headMaybe (x:_) = Just x
 
-replaceBufstack :: [Nvim.Buffer] -> BufstackM ()
+replaceBufstack :: [Buffer] -> BufstackM ()
 replaceBufstack newBuffers = modifyBuffersM_ (const newBuffers)
 
 reverseBufstack :: BufstackM ()
 reverseBufstack = modifyBuffersM_ reverse
 
-nextBufFunction :: BufstackM ()
-nextBufFunction = undefined  -- TODO
+nextBufFunction :: BufstackMEither ()
+nextBufFunction = do
+        numberedBufs <- fmapNvimEither (map (\x -> (,) x <$> getNumber x) . nub) nvim_list_bufs
+        currentBuf <- nvim_get_current_buf
+        currentBufNum <- getNumber currentBuf
+
+        let headMaybe [] = Nothing
+            headMaybe (x:_) = Just x
+
+            atMaybe xs i
+                    | (length xs) > (i + 1) = Nothing
+                    | otherwise = Just (xs !! i)
+
+            f = do -- Either monad
+               sortedNumberedBufs <- sortBy (compare `on` fst) <$> numberedBufs
+               currentBuf' <- currentBuf
+               currentBufNum' <- currentBufNum
+
+               currentBufIndex <- maybe (ErrorMessage "Could not find current buf index") return .
+                                       findIndex (== currentBufNum') $ sortedNumberedBufs
+
+               let nextBufIndex = currentBufIndex + 1
+
+                   -- make sure we don't try to switch to the current buf
+                   filterCurrentBuf (Just x) = if x == currentBuf' then Nothing
+                                                                   else Just x
+                   filterCurrentBuf Nothing = Nothing
+
+
+               return . filterCurrentBuf . fmap fst $ 
+                    if nextBufIndex >= (length sortedNumberedBufs)
+                        -- should never return Nothing, but just in case
+                        then headMaybe sortedNumberedBufs 
+                        else atMaybe nextBufIndex sortedNumberedBufs
+
+        case f of Right (Nothing) -> return . return $ () -- there's only 1 buffer, do nothing
+                  Right (Just nextBuf) -> pushBuffer nextBuf >> nvim_set_current_buf' nextBuf
+                  Left x -> return . return $ x
+
 
 -- | TODO: use the previous buffer (numerically) if the stack is empty
 -- (i.e. if popBuffer returns Nothing)
 previousBufFunction :: BufstackM ()
 previousBufFunction = popBuffer >>= f
-    where f (Just buf) = Nvim.nvim_set_current_buf' buf
+    where f (Just buf) = nvim_set_current_buf' buf
           f _ = return ()
