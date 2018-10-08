56e586d85d1b5298091ed5a5f00bbf72ce3c83c1
Thomas Jakway <tjakway@nyu.edu>
2018-10-08 10:34:06 -0400

    fixup! reorganizing config and error handling

--- src/Bufstack/Error.hs
+++ src/Bufstack/Error.hs
@@ -7,24 +7,16 @@ import System.IO
 import Bufstack.Core
 import Bufstack.Config.Type
 
-throwOnError :: NeovimException -> Neovim env ()
-throwOnError = return . throw . pretty
-
 printStderrOnError :: NeovimException -> IO ()
-printStderrOnError = hPutStrLn stderr . show . pretty 
-
-onErrorM :: NeovimException -> BufstackM ()
-onErrorM e = do
-        onErr <- onErrorFIO . config <$> ask
-        liftIO $ onErr e
-
-        
+printStderrOnError = hPutStrLn stderr . show . pretty . show 
 
 wrapErrors :: [NeovimException] -> NeovimException
-wrapErrors exs = ErrorMessage $ (pretty "Multiple errors: ") <> (pretty exs)
+wrapErrors exs = ErrorMessage $ (pretty "Multiple errors: ") <> (pretty . show $ exs)
 
 handleError :: NeovimException -> BufstackM ()
-handleError = liftIO . onErrorFIO . config <$> ask
+handleError e = do
+        onErr <- onErrorFIO . config <$> ask
+        liftIO $ onErr e
 
 handleErrorE :: Either NeovimException a -> BufstackM ()
 handleErrorE (Right _) = return ()
@@ -33,12 +25,10 @@ handleErrorE (Left e) = handleError e
 handleErrorME :: BufstackM (Either NeovimException a) -> BufstackM ()
 handleErrorME e = e >>= handleErrorE
 
-printAndThrow :: NeovimException -> BufstackM ()
-printAndThrow e =  printStderrOnError' e >> throwN e
-    where throwN = liftIO throw
-          printStderrOnError' = liftIO printStderrOnError
+printAndThrow :: NeovimException -> IO ()
+printAndThrow e =  printStderrOnError e >> throw e
 
 
 
-defaultOnError :: NeovimException -> BufstackM ()
+defaultOnError :: NeovimException -> IO ()
 defaultOnError = printAndThrow
