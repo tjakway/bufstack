2c42e4844c4dd01ba309af9904a15518575186af
Thomas Jakway <tjakway@nyu.edu>
2018-10-08 11:07:21 -0400

    wrote assertPeekBufstack and assertBufstack

--- test/Bufstack/Test/Vim/Utils.hs
+++ test/Bufstack/Test/Vim/Utils.hs
@@ -6,6 +6,9 @@ import Neovim
 
 -- lifted hunit assertions
 
+assertFail :: String -> Neovim env ()
+assertFail = liftIO . Hunit.assertFail
+
 assertEqual :: (Eq a, Show a) => String -> a -> a -> Neovim env ()
 assertEqual a b = liftIO . HUnit.assertEqual a b
 
@@ -19,3 +22,19 @@ assertCurrentBufEquals :: Buffer -> Neovim env ()
 assertCurrentBufEquals expected = do
         currentBuf <- vim_get_current_buffer'
         assertEqual "Assert current buf equals" expected currentBuf
+
+-- | check that the top of the bufstack is equal to expected
+assertPeekBufstack :: Buffer -> BufstackM ()
+assertPeekBufstack expected = do
+        let msg = "Assert top of bufstack == " ++ show expected
+        top <- peekBuffer
+        case top of Just x -> assertEqual msg expected x
+                    Nothing -> assertFail msg
+
+-- | check that the underlying bufstack is equal to expected
+assertBufstack :: [Buffer] -> BufstackM ()
+assertBufstack expected = 
+        let msg = "Check bufstack is equal to expected"
+            in getBuffers >>= assertEqual msg expected
+
+
