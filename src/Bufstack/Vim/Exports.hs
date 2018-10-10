601fbee1c292f66aea29d6a566f83e364e3f1257
Thomas Jakway <tjakway@nyu.edu>
2018-10-09 21:02:58 -0400

    fixed Exports.plugin

--- src/Bufstack/Vim/Exports.hs
+++ src/Bufstack/Vim/Exports.hs
@@ -5,11 +5,21 @@ import Neovim
 
 import qualified Neovim.Plugin.Internal as Nvim
 
-import Bufstack.Core
+import qualified Bufstack.Core as Bufstack
+import qualified Bufstack.Config.Default as Bufstack
+import Bufstack.Util
 import Bufstack.BufferOperations
 import Bufstack.Vim.Autocmds
 
-exports :: [Nvim.ExportedFunctionality Bufstack]
-exports = [$(function' 'nextBufFunction) Async,
+exportedFunctions :: [Nvim.ExportedFunctionality Bufstack.Bufstack]
+exportedFunctions = [$(function' 'nextBufFunction) Async,
           $(function' 'prevBufFunction) Async,
           $(function' 'printBufstack) Async]
+
+plugin :: Neovim (StartupConfig NeovimConfig) NeovimPlugin
+plugin = do
+        env <- atomically $ Bufstack.initBufstack Bufstack.defaultConfig 
+        wrapPlugin (Plugin
+            { environment = env
+            , exports     = exportedFunctions
+            })
