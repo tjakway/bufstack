11b68694e1a6fedb4d88c48824e399e328350708
Thomas Jakway <tjakway@nyu.edu>
2018-10-09 20:47:08 -0400

    added exported function list

--- src/Bufstack/Vim/Exports.hs
+++ src/Bufstack/Vim/Exports.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Bufstack.Vim.Exports where
 
+import Neovim
+
 import qualified Neovim.Plugin.Internal as Nvim
+
+import Bufstack.Core
+import Bufstack.BufferOperations
 import Bufstack.Vim.Autocmds
 
-exports :: [Nvim.ExportedFunctionality a]
-exports = []
+exports :: [Nvim.ExportedFunctionality Bufstack]
+exports = [$(function' 'nextBufFunction) Async,
+          $(function' 'prevBufFunction) Async,
+          $(function' 'printBufstack) Async]
