f84f8aa24ccaf37f7ce95ae54a2954197162a3a0
Thomas Jakway <tjakway@nyu.edu>
2018-10-10 09:11:42 -0400

    added logging options

--- src/exe/StandaloneExe.hs
+++ src/exe/StandaloneExe.hs
@@ -1,10 +1,18 @@
-import System.IO
+module Main where
 
 import Neovim
 import qualified Bufstack.Vim.Exports as Bufstack
 
+import System.Directory
+import System.FilePath
 
 main :: IO ()
-main = neovim defaultConfig
-        { plugins = [ Bufstack.plugin ] ++ plugins defaultConfig
-        }
+main = do
+        let logFile = "bufstack.log"
+            logLevel = DEBUG
+
+        logPath <- (</> logFile) <$> getCurrentDirectory
+        neovim defaultConfig
+            { plugins = [ Bufstack.plugin ] ++ plugins defaultConfig,
+              logOptions = Just (logFile, logLevel)
+            }
